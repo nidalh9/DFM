1bd1692a73e7fa9cb25dcd258f1f4c6a734b3b81
Nidal Hulaihel <nidal@apple.com>
2024-01-01 23:33:32 +0200

    temp

--- denoising_diffusion_pytorch/denoising_diffusion_pytorch.py
+++ denoising_diffusion_pytorch/denoising_diffusion_pytorch.py
@@ -103,7 +103,7 @@ class GaussianDiffusion(nn.Module):
         self,
         model,
         image_size,
-        timesteps=1000,
+        timesteps=10,
         sampling_timesteps=None,
         loss_type="l1",
         objective="pred_noise",
@@ -1100,7 +1100,7 @@ class Trainer(object):
 
         # model = self.accelerator.unwrap_model(self.model)
         model = self.model
-        print(f"model parameter names: {list(model.state_dict().keys())}")
+        # print(f"model parameter names: {list(model.state_dict().keys())}")
         # load all parameteres
         data["model"].pop("model.enc.pos_embed")
         print(model.load_state_dict(data["model"], strict=False))
